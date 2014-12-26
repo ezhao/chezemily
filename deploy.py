311507387db1fab88e94f8064bcdc06e5fc9a5ba
Emily Zhao <emily@dropbox.com>
2014-12-25 16:25:24 -0800

    debugging continues

--- deploy.py
+++ deploy.py
@@ -1,6 +1,19 @@
-from hello import app
-from setlist.setlist import setlist
+from flask import Flask
+from flask import render_template, request, session, g, redirect, url_for, abort, flash
+import os
+from flask.ext.sqlalchemy import SQLAlchemy
+from datetime import datetime
 
-app.register_blueprint(setlist, url_prefix='/setlist')
-#app.debug = False # Only deploy with "False"
-app.run()
+
+# Create application
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+app.config.update(dict(
+    SQLALCHEMY_DATABASE_URI=os.environ['DATABASE_URL'],
+    SECRET_KEY='2g2JXhqfutWCzRBAnuUpTRVNoneadventureatatimebaconandburrata',
+    USERNAME='snowman',
+    PASSWORD='pumpkin',
+    PC_NAME=['peter', 'moop', 'peter cottle', 'peter michael cottle'],
+))
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
--- hello.py
+++ hello.py
@@ -3,20 +3,7 @@ from flask import render_template, request, session, g, redirect, url_for, abort
 import os
 from flask.ext.sqlalchemy import SQLAlchemy
 from datetime import datetime
-
-# Create application
-app = Flask(__name__)
-app.config.from_object(__name__)
-
-app.config.update(dict(
-    SQLALCHEMY_DATABASE_URI=os.environ['DATABASE_URL'],
-    SECRET_KEY='2g2JXhqfutWCzRBAnuUpTRVNoneadventureatatimebaconandburrata',
-    USERNAME='snowman',
-    PASSWORD='pumpkin',
-    PC_NAME=['peter', 'moop', 'peter cottle', 'peter michael cottle'],
-))
-app.config.from_envvar('FLASKR_SETTINGS', silent=True)
-
+from deploy import app
 
 ############################
 # Database related stuff   #
